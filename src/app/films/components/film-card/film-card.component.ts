17bfdb739b9ac9114b5e1f80ceb0e88ad9eafc36
savryna <assavr0@gmail.com>
2026-05-01 21:03:56 +0500

    feat: add router link for card component

--- src/app/films/components/film-card/film-card.component.ts
+++ src/app/films/components/film-card/film-card.component.ts
@@ -1,15 +1,17 @@
 import type { InputSignal } from '@angular/core';
 import { ChangeDetectionStrategy, Component, input } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 import type { FilmData } from '../../models/films.model';
 
 @Component({
   selector: 'app-film-card',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './film-card.component.html',
   styleUrl: './film-card.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class FilmCardComponent {
   public filmData: InputSignal<FilmData> = input.required();
+  protected readonly String = String;
 }
