ffa53ae7c9936da9271b47b6352f7ad7640b54b6
savryna <assavr0@gmail.com>
2026-05-03 00:05:39 +0500

    fix: remove pipe for search and add computer signal for search

--- src/app/films/components/film-list/film-list.component.ts
+++ src/app/films/components/film-list/film-list.component.ts
@@ -1,15 +1,15 @@
-import { ChangeDetectionStrategy, Component, inject, signal } from '@angular/core';
+import type { WritableSignal } from '@angular/core';
+import { ChangeDetectionStrategy, Component, computed, inject, signal } from '@angular/core';
 import { Router } from '@angular/router';
 
 import type { FilmData } from '../../models/films.model';
-import { SearchByWorldPipe } from '../../pipes/search-by-world-pipe';
 import { FilmService } from '../../services/film.service';
 import { FilmCardComponent } from '../film-card/film-card.component';
 import { SearchInputComponent } from '../search-input/search-input.component';
 
 @Component({
   selector: 'app-film-list',
-  imports: [FilmCardComponent, SearchInputComponent, SearchByWorldPipe],
+  imports: [FilmCardComponent, SearchInputComponent],
   templateUrl: './film-list.component.html',
   styleUrl: './film-list.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -20,7 +20,9 @@ export class FilmListComponent {
 
   public readonly films = this.filmService.films;
 
-  public readonly searchWord = signal('');
+  public readonly searchWord: WritableSignal<null | string> = signal(null);
+
+  public readonly filteredFilms = computed(() => this.filterFilms());
 
   public redirectDetailsPage(film: FilmData): void {
     void this.router.navigate(['/film/', film.id], {
@@ -37,5 +39,11 @@ export class FilmListComponent {
 
   public setSearchWord(word: string): void {
     this.searchWord.set(word);
+  }
+
+  private filterFilms(): FilmData[] {
+    const searchWord = this.searchWord()?.toLowerCase() ?? '';
+
+    return this.films().filter((film) => film.title.toLowerCase().includes(searchWord));
   }
 }
