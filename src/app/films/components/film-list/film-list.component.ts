0335363d47ef465caa3579e51c7876bc65590098
savryna <assavr0@gmail.com>
2026-05-02 21:06:32 +0500

    feat: add search by word

--- src/app/films/components/film-list/film-list.component.ts
+++ src/app/films/components/film-list/film-list.component.ts
@@ -1,13 +1,15 @@
-import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, signal } from '@angular/core';
 import { Router } from '@angular/router';
 
 import type { FilmData } from '../../models/films.model';
+import { SearchByWorldPipe } from '../../pipes/search-by-world-pipe';
 import { FilmService } from '../../services/film.service';
 import { FilmCardComponent } from '../film-card/film-card.component';
+import { SearchInputComponent } from '../search-input/search-input.component';
 
 @Component({
   selector: 'app-film-list',
-  imports: [FilmCardComponent],
+  imports: [FilmCardComponent, SearchInputComponent, SearchByWorldPipe],
   templateUrl: './film-list.component.html',
   styleUrl: './film-list.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -17,6 +19,8 @@ export class FilmListComponent {
   private readonly router = inject(Router);
 
   public readonly films = this.filmService.films;
+
+  public readonly searchWord = signal('');
 
   public redirectDetailsPage(film: FilmData): void {
     void this.router.navigate(['/film/', film.id], {
@@ -29,5 +33,9 @@ export class FilmListComponent {
         filmTitle: film.title,
       },
     });
+  }
+
+  public setSearchWord(word: string): void {
+    this.searchWord.set(word);
   }
 }
--- src/app/films/components/search-input/search-input.component.ts
+++ src/app/films/components/search-input/search-input.component.ts
@@ -1,10 +1,30 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import type { OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, output } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { debounceTime, distinctUntilChanged } from 'rxjs';
 
 @Component({
   selector: 'app-search-input',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './search-input.component.html',
   styleUrl: './search-input.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class SearchInputComponent {}
+export class SearchInputComponent implements OnInit {
+  public searchForm: FormGroup;
+  public readonly searchWordChange = output<string>();
+
+  constructor() {
+    this.searchForm = new FormGroup({
+      searchWord: new FormControl(''),
+    });
+  }
+
+  public ngOnInit(): void {
+    this.searchForm.controls['searchWord'].valueChanges
+      .pipe(debounceTime(500), distinctUntilChanged())
+      .subscribe((filterWord) => {
+        this.searchWordChange.emit(filterWord);
+      });
+  }
+}
