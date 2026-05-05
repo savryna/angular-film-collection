7d1a874acd93ac048ac4d46f329a6da8dbbc8a48
savryna <assavr0@gmail.com>
2026-05-05 21:57:21 +0500

    refactor: move search input component from film-list component to film-list-page component

--- src/app/films/pages/film-list/film-list-page.component.ts
+++ src/app/films/pages/film-list/film-list-page.component.ts
@@ -1,12 +1,30 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, computed, inject, signal } from '@angular/core';
 
 import { FilmListComponent } from '../../components/film-list/film-list.component';
+import { SearchInputComponent } from '../../components/search-input/search-input.component';
+import type { FilmData } from '../../models/films.model';
+import { FilmService } from '../../services/film.service';
 
 @Component({
   selector: 'app-film-list-page',
-  imports: [FilmListComponent],
+  imports: [FilmListComponent, SearchInputComponent],
   templateUrl: './film-list-page.component.html',
   styleUrl: './film-list-page.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class FilmListPageComponent {}
+export class FilmListPageComponent {
+  private readonly filmService = inject(FilmService);
+  public readonly searchWord = signal<string | null>(null);
+  public readonly filteredFilms = computed(() => this.filterFilms());
+  public readonly films = this.filmService.films;
+
+  public setSearchWord(word: string): void {
+    this.searchWord.set(word);
+  }
+
+  private filterFilms(): FilmData[] {
+    const searchWord = this.searchWord()?.toLowerCase() ?? '';
+
+    return this.films().filter((film) => film.title.toLowerCase().includes(searchWord));
+  }
+}
